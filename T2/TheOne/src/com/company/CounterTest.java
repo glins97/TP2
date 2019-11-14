ef92326c45ce3bfe7544881d7ce3ffa5ced984ec
Gabriel Lins <gabriel.lins97@gmail.com>
2019-11-13 21:48:21 -0300

    Adição de novos testes unitários

--- T2/TheOne/src/com/company/CounterTest.java
+++ T2/TheOne/src/com/company/CounterTest.java
@@ -3,6 +3,7 @@ package com.company;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -11,14 +12,18 @@ public class CounterTest {
 
     @Test
     public void getFrequencyList() {
-        // Case test
-        List<String> words = Arrays.asList("Gabriel", "Gabriel", "Matheus", "");
-        assert new Counter().getFrequencyList(words).get("Gabriel") == 2;
-        assert new Counter().getFrequencyList(words).get("Matheus") == 1;
-        assert new Counter().getFrequencyList(words).get("") == 1;
+        Counter counter = new Counter();
 
         // Empty words test
-        List<String> words2 = Arrays.asList();
-        assert new Counter().getFrequencyList(words2).keySet().size() == 0;
+        List<String> words2 = Collections.emptyList();
+        assert counter.getFrequencyList(words2).keySet().size() == 0;
+
+        // Case test
+        List<String> words = Arrays.asList("Gabriel", "Gabriel", "Matheus", "", "", "gabriel");
+        assert counter.getFrequencyList(words).get("Gabriel") == 2;
+        assert counter.getFrequencyList(words).get("Matheus") == 1;
+        assert counter.getFrequencyList(words).get("") == 2;
+        assert counter.getFrequencyList(words).get("gabriel") == 1;
+
     }
 }
--- T2/TheOne/src/com/company/InputTest.java
+++ T2/TheOne/src/com/company/InputTest.java
@@ -11,6 +11,46 @@ import static org.junit.Assert.*;
 public class InputTest {
 
     @Test
+    public void filterWords() {
+        // Check for special chars before and after words
+        List<String> words = new ArrayList<String>(Arrays.asList("a", " a", "!a", "$a", "(a)", "a.", "a'", "$!@#$%"));
+        words = new Input().filterWords(words);
+        List<String> check = Arrays.asList("a", "a", "a", "a", "a", "a", "a", "");
+
+        // For each item, check if it's the expected value
+        for (int i=0; i < words.size(); i++){
+            assert words.get(i).equals(check.get(i));
+        }
+    }
+
+    @Test
+    public void normalizeWords() {
+        // Check if words are normalized
+        List<String> words = Arrays.asList("AaA", "a", "", "A", "AA");
+        words = new Input().normalizeWords(words);
+        List<String> check = Arrays.asList("aaa", "a", "", "a", "aa");
+
+        // For each item, check if it's the expected value
+        for (int i=0; i < words.size(); i++){
+            assert words.get(i).equals(check.get(i));
+        }
+    }
+
+    @Test
+    public void removeStopWords() {
+        // Removes stop words
+        List<String> words = new ArrayList<String>(Arrays.asList("a", "b", "c", "d", "e"));
+        List<String> stopWords = Arrays.asList("c", "d");
+        words = new Input().removeStopWords(words, stopWords);
+        List<String> check = Arrays.asList("a", "b", "e");
+
+        // For each item, check if it's the expected value
+        for (int i=0; i < words.size(); i++){
+            assert words.get(i).equals(check.get(i));
+        }
+    }
+
+    @Test
     public void loadWords() {
         List<String> words = new Input().loadWords("names");
         List<String> check = Arrays.asList("gabriel", "gabriel", "gabriel", "gabriel", "matheus",
@@ -19,34 +59,4 @@ public class InputTest {
         assert words.equals(check);
     }
 
-    @Test
-    public void filterWords() {
-        List<String> words = new ArrayList(Arrays.asList("a", " a", "!a", "$a", "(a)", "a.", "a'", "$!@#$%"));
-        words = new Input().filterWords(words);
-        List<String> check = Arrays.asList("a", "a", "a", "a", "a", "a", "a", "");
-        for (int i=0; i < words.size(); i++){
-            assert words.get(i).equals(check.get(i));
-        }
-    }
-
-    @Test
-    public void normalizeWords() {
-        List<String> words = Arrays.asList("AaA", "a", "", "A", "AA");
-        words = new Input().normalizeWords(words);
-        List<String> check = Arrays.asList("aaa", "a", "", "a", "aa");
-        for (int i=0; i < words.size(); i++){
-            assert words.get(i).equals(check.get(i));
-        }
-    }
-
-    @Test
-    public void removeStopWords() {
-        List<String> words = new ArrayList(Arrays.asList("a", "b", "c", "d", "e"));
-        List<String> stopWords = Arrays.asList("c", "d");
-        words = new Input().removeStopWords(words, stopWords);
-        List<String> check = Arrays.asList("a", "b", "e");
-        for (int i=0; i < words.size(); i++){
-            assert words.get(i).equals(check.get(i));
-        }
-    }
 }
